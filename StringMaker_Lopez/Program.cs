14630f56b98554eaf93f80036994b34cbaa252f6
Clarissa Lopez <102737214+lopezclarissa2021@users.noreply.github.com>
2023-10-20 10:08:44 -0700

    Add project files.

---
+++ StringMaker_Lopez/Program.cs
@@ -0,0 +1,27 @@
+﻿// Clarissa Lopez
+// IT113
+// NOTES: notes the instructor should read
+// BEHAVIORS NOT IMPLIMENTED AND WHY: 
+namespace StringMaker_Lopez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public string Reverse(string s)
+            {
+                Stack<int> stack = new Stack<int>();
+             for(int i = 0; i < s.Length; i++)
+                {
+                    stack.Push(i);
+                }
+                return s;
+            }
+
+            public string Reverse(string s, bool PreserveCaseLocation)
+            {
+
+            }
+        }
+    }
+}
