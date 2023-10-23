52148f107b16e2fe598710a37dcab7e65b70bbe5
Clarissa Lopez <102737214+lopezclarissa2021@users.noreply.github.com>
2023-10-22 18:01:09 -0700

    Finsihed the toString method

--- StringMaker_Lopez/Program.cs
+++ StringMaker_Lopez/Program.cs
@@ -2,8 +2,17 @@
 // IT113
 // NOTES: notes the instructor should read
 // BEHAVIORS NOT IMPLIMENTED AND WHY: 
+
 using StringMaker_Lopez;
 
 StringManager sm = new StringManager();
 
-Console.WriteLine(sm.Reverse("hello"));   
+Console.WriteLine($"Reversed: {sm.Reverse("Sunbeam Tiger")}");
+Console.WriteLine($"ToString: {sm.ToString()}");
+Console.WriteLine($"Reversed Preserve Case: {sm.Reverse("Sunbeam Tiger", true)}");
+Console.WriteLine($"Equals: {sm.Equals("Sunbeam Tiger")}");
+Console.WriteLine($"Symmetric: {sm.Symmetric("ABBA")}");
+Console.WriteLine($"Symmetric: {sm.Symmetric("ABA")}");
+Console.WriteLine($"Symmetric: {sm.Symmetric("ABba")}");
+Console.WriteLine(sm.Reverse("heLlo WorlD"));   
+Console.WriteLine(sm.Reverse("heLlo WorlD", true));
--- StringMaker_Lopez/StringManager.cs
+++ StringMaker_Lopez/StringManager.cs
@@ -8,8 +8,10 @@ namespace StringMaker_Lopez
 {
     internal class StringManager
     {
+        public string CurrInput { get; set; } = "";
         public string Reverse(string s)
         {
+            CurrInput = s;
             string revString = "";
             Stack<char> stack = new Stack<char>();
             for (int i = 0; i < s.Length; i++)
@@ -25,9 +27,63 @@ namespace StringMaker_Lopez
             return revString;
         }
 
-        //public string Reverse(string s, bool PreserveCaseLocation)
-        //{
+        public string Reverse(string s, bool PreserveCaseLocation)
+        {
+            string revStr = Reverse(s);
+            string newStr = "";
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(char.IsWhiteSpace(s[i]))
+                {
+                    newStr += revStr[i].ToString();
+                }else if (s[i].ToString().ToUpper() == s[i].ToString())
+                {
+                    newStr += revStr[i].ToString().ToUpper();
+                }else
+                {
+                    newStr += revStr[i].ToString().ToLower();
+                }
+            }
+            return newStr;  
 
-       // }
+        }
+
+        public bool Symmetric(string s)
+        {
+            CurrInput = s;
+            return s == Reverse(s);
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(CurrInput))
+            {
+                return "Negative One";
+            }
+
+            int asciiTotal = 0;
+            for (int i = 0; i < CurrInput.Length; i++)
+            {
+                asciiTotal += Convert.ToInt32(CurrInput[i]);
+            }
+
+            string numberString = asciiTotal.ToString();
+
+            string resultString = "";
+            List<string> numList = new List<string> { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight","Nine" };
+
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                resultString += numList[int.Parse(numberString[i].ToString())] + " ";
+            }
+
+            return resultString.Trim();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return CurrInput.Equals(obj);
+        }
+
     }
 }
